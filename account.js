32c03814e4817a6da3b08bc4e96a75141d993d2c
Nguyen Ngoc Xuan My <nnxmy97@gmail.com>
2019-07-29 07:04:07 +0700

    sort list of accounts

--- account.js
+++ account.js
@@ -1,31 +1,26 @@
 
 const list = [
     { credit: 1234, debit: null, accountNumber: 20055 },
-    { credit: null, debit: 2034, accountNumber: 20053 },
+    { credit: null, debit: 1230, accountNumber: 45503 },
     { credit: null, debit: null, accountNumber: 45652 },
-    { credit: 3224, debit: null, accountNumber: 63223 },
-    { credit: null, debit: 1230, accountNumber: 45503 }
+    { credit: null, debit: 2034, accountNumber: 20053 },
+    { credit: 3224, debit: null, accountNumber: 63223 }
 ];
 
-function getCreditAccounts(arr){
-    var creditArray = arr.filter(x => x.credit !=null)
-    return creditArray.sort((a, b) => (a.credit < b.credit) ? 1 : -1);
+function sortByCredit(list){
+    return list.sort((a, b) => (a.credit < b.credit) ? 1 : -1);
 }
 
-function getDebitAccounts(arr){
-    var debitArray = arr.filter(x => x.debit !=null)
-    return debitArray.sort((a, b) => (a.debit < b.debit) ? 1 : -1);
+function sortByDebit(list){
+    return list.sort((a, b) => (a.debit < b.debit) ? 1 : -1);
 }
 
-function getUndefinedAccounts(arr){
-    var justAccountNumberArray = arr.filter(x => x.credit == null && x.debit == null)
-    return justAccountNumberArray.sort((a, b) => (a.accountNumber < b.accountNumber) ? 1 : -1);
+function sortUndefiedAccounts(list){
+    return list.sort((a, b) => (a.accountNumber < b.accountNumber) ? 1 : -1);
 }
 
-function sorting(arr){
-    var creditAccounts = getCreditAccounts(list);
-    var debitAccounts = getDebitAccounts(list);
-    var undefinedAccouns = getUndefinedAccounts(list);
-    return creditAccounts.concat(debitAccounts, undefinedAccouns);
+function sortingAccountsList(list){
+    return sortByCredit(sortByDebit(sortUndefiedAccounts(list)));
 }
-console.log(sorting(list));
+
+console.log(sortingAccountsList(list));
